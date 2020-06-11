1f9a0bee6bd0e062453f24744977fe9a34af95d7
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-11 21:59:15 +0200

    Add ProductsController

---
+++ src/Services/Catalog/Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.Models;
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Catalog.API.Data;
+using ApiLibrary.Core.Entities;
+
+namespace Catalog.API.Controllers
+{
+    [ApiVersion("1")]
+    public class ProductsController : BaseController<Product, int, CatalogDbContext>
+    {
+
+        public ProductsController(CatalogDbContext db) : base (db)
+        {
+           
+        }
+
+        public override Task<ActionResult> DeleteItemAsync([FromRoute] int id)
+        {
+            return base.DeleteItemAsync(id);
+        }
+
+        public override Task<ActionResult<Product>> GetItemByIdAsync([FromRoute] object id)
+        {
+            return base.GetItemByIdAsync(id);
+        }
+
+        public override Task<ActionResult> PostItemAsync([FromBody] Product item)
+        {
+            return base.PostItemAsync(item);
+        }
+
+        public override Task<ActionResult> PutItemAsync([FromBody] Product item, [FromRoute] int id)
+        {
+            return base.PutItemAsync(item, id);
+        }
+    }
+}
