4701b9f7f4fa4812edc263f953f0d17daef3bf5c
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-11 21:56:28 +0200

    Add CatalogDbContext

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Models;
+using ApiLibrary.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Data
+{
+    public class CatalogDbContext : BaseDbContext
+    {
+
+        public CatalogDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+       public DbSet<Product> Products { get; set; }
+       public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
