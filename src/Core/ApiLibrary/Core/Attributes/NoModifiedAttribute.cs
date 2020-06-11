bcd25653ce377f221e630d236a7cefd08a1bcf14
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-11 21:59:44 +0200

    Add NoModifiedAttribute

---
+++ src/Core/ApiLibrary/Core/Attributes/NoModifiedAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiLibrary.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NoModifiedAttribute : Attribute
+    {
+    }
+}
