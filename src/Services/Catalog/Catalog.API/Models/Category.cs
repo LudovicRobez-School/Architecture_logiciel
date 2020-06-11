089d3099a66cef2a9d1f269eee9240c747a8afc9
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-11 22:02:18 +0200

    Add Category Model

---
+++ src/Services/Catalog/Catalog.API/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ApiLibrary.Core.Entities;
+
+namespace Catalog.API.Models
+{
+    public class Category : BaseModel<int>
+    {
+        [StringLength(150)]
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
