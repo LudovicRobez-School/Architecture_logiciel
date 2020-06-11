4b2de62f72bc9f1a9cf186d4e7c97bde15f05dbc
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-11 21:58:05 +0200

    Add BaseModel

---
+++ src/Core/ApiLibrary/Core/Entities/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiLibrary.Core.Attributes;
+
+namespace ApiLibrary.Core.Entities
+{
+    public class BaseModel<T> : BaseEntity
+    {
+        [NoModified]
+        public T Id { get; set; }
+    }
+}
