398fd09414d6b3a951fb369fa849eb8bd3f5a087
LudovicRobez <ludovic.robez@hotmail.fr>
2020-06-11 21:57:52 +0200

    Add BaseEntity

---
+++ src/Core/ApiLibrary/Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiLibrary.Core.Attributes;
+
+namespace ApiLibrary.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        [NoModified]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        [NoModified]
+        public DateTime? DeletedAt { get; set; }
+    }
+}
